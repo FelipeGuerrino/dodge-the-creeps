87fc036fceaa56b4c6a9cec57710b658cd8e532a
FelipeGuerrino <felipe_bg2005@hotmail.com>
2023-09-16 02:26:47 -0300

    player movement

---
+++ Player.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Signal]
+	public delegate void HitEventHandler();
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public Vector2 ScreenSize;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero;
+
+		Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		velocity = inputDirection;
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
+		Hide();
+	}
+}
